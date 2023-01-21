e56c1059fdca0deae491744a1d61284ec7b91ff1
Franco Galvano <franco_millo11@hotmail.com>
2023-01-21 19:19:05 -0300

    version 2

---
+++ js/function.js
@@ -0,0 +1,30 @@
+function verMas (){
+    document.getElementById('uniBorcelle').style.display = 'block';
+}
+function verMenos () {
+    document.getElementById('uniBorcelle').style.display= 'none';
+}
+
+function verMas2 (){
+    document.getElementById('belgrano').style.display = 'block';
+}
+function verMenos2 () {
+    document.getElementById('belgrano').style.display= 'none';
+}
+
+function verMas3 (){
+    document.getElementById('hotel').style.display = 'block';
+    
+}
+function verMenos3 () {
+    document.getElementById('hotel').style.display= 'none';
+}
+
+function verMas4 (){
+    document.getElementById('target').style.display = 'block';
+
+}
+function verMenos4 () {
+    document.getElementById('target').style.display= 'none';
+}
+
